37d66a2f75ce72ab8e0ef200b956235408a65ca4
ariefsetyan <ariefsetyanugraha@ymail.com>
2021-01-31 22:57:20 +0700

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,21 @@
+require('./bootstrap');
+
+// window.Vue = require('vue').default;
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+import {routes} from './routes';
+
+const router = new VueRouter({
+    routes, // short for `routes: routes`
+    mode: 'history'
+})
+
+// Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
+
+const app = new Vue({
+    el: '#app',router
+});
